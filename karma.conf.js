f2124628e526d31510079eb7bb28f00c80129f4b
JuanNicolasMalagon <nicolas0915@hotmail.com>
2025-10-27 19:31:11 -0500

    Update Karma configuration, package.json, and CI workflow
    - Simplified the Karma configuration by removing unnecessary comments and streamlining the setup.
    - Updated the test:ci script in package.json to remove the browser timeout setting.
    - Changed the Node.js version in the CI workflow to 20.19 and added an environment variable for headless Chrome testing.

--- karma.conf.js
+++ karma.conf.js
@@ -1,40 +1,25 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
-    basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
+      require('@angular-devkit/build-angular/plugins/karma'),
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('karma-coverage')
     ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
-    },
-    reporters: ['progress', 'kjhtml'],
-    browsers: ['ChromeHeadless'],
     customLaunchers: {
       ChromeHeadless: {
         base: 'ChromeHeadless',
         flags: [
           '--no-sandbox',
           '--disable-setuid-sandbox',
-          '--disable-dev-shm-usage',
-          '--disable-accelerated-2d-canvas',
-          '--no-first-run',
-          '--no-zygote',
-          '--disable-gpu'
+          '--disable-dev-shm-usage'
         ]
       }
     },
-    restartOnFileChange: true
+    browsers: ['ChromeHeadless'],
+    reporters: ['progress', 'kjhtml']
   });
 };
 
