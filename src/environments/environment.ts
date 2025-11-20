d37879cc26a132d8712581e78be668022ea96658
Juan Nicolás Malagón <jn.malagon@uniandes.edu.co>
2025-11-19 21:16:23 -0500

    Remove Google Maps API key from environment
    Removed Google Maps API key for security reasons.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   baseApiUrl: 'http://localhost:8080',
   version: '1.1.0',
-  googleMapsApiKey: 'AIzaSyCjrvm9MQXOthvDC06729Drv-z5THAJNH8',
+  googleMapsApiKey: '',
   googleMapsMapId: 'medisupplyg4-development'
 };
 
